069db8e6eb98f4e86c3e9f570ac28e697ea364df
Travis Swicegood <development@domain51.com>
2011-05-20 17:15:00 -0500

    first pass at some output for a "project" template in Armstrong

---
+++ armstrong/cli/templates/standard/config/production.py
@@ -0,0 +1,23 @@
+from config.defaults import *
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+# Change this to work with your default production database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# Attempt to load any settings from config.local_development, but ignore any
+# errors complaining about them not being present.
+try:
+    from config.local_production import *
+except ImportError, e:
+    pass
---
+++ armstrong/cli/templates/standard/wsgi.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+# Change this to reflect the environment you're in
+os.environ['DJANGO_SETTINGS_MODULE'] = 'config.development'
+
+# Make sure that this project directory is first in the sys.path, or at least
+# as close to first as can be reasonably assured in Python.
+sys.path.insert(0, os.path.dirname(__file__))
+
+from django.core.handlers.wsgi import WSGIHandler
+application = WSGIHandler()
