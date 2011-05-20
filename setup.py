007c23a2bfbcc50802fea3adc483c5d5afd9b8a5
Travis Swicegood <development@domain51.com>
2011-05-20 15:54:18 -0500

    initial skeleton

---
+++ setup.py
@@ -0,0 +1,61 @@
+from distutils.core import setup
+import os
+
+if os.path.exists("MANIFEST"):
+    os.unlink("MANIFEST")
+
+# Borrowed and modified from django-registration
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir:
+    os.chdir(root_dir)
+
+
+def build_package(dirpath, dirnames, filenames):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'):
+            del dirnames[i]
+    if '__init__.py' in filenames and 'steps.py' not in filenames:
+        pkg = dirpath.replace(os.path.sep, '.')
+        if os.path.altsep:
+            pkg = pkg.replace(os.path.altsep, '.')
+        packages.append(pkg)
+    elif filenames:
+        prefix = dirpath[10:]  # Strip "armstrong<dir separator>"
+        for f in filenames:
+            # Ignore all dot files and any compiled
+            if f.startswith(".") or f.endswith(".pyc"):
+                continue
+            data_files.append(os.path.join(prefix, f))
+
+
+[build_package(dirpath, dirnames, filenames) for dirpath, dirnames, filenames
+        in os.walk('armstrong/cli')]
+
+
+setup(
+    name='armstrong.cli',
+    version='0.0.1.alpha.0',
+    description='Provides basic commands needed for Armstrong scaffolding',
+    author='Bay Citizen & Texas Tribune',
+    author_email='dev@armstrongcms.org',
+    url='http://github.com/armstrongcms/armstrong.cli/',
+    packages=packages,
+    package_data={
+        "armstrong": data_files,
+        "": ["README.rst", ],
+    },
+    namespace_packages=["armstrong", "armstrong.cli", ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
