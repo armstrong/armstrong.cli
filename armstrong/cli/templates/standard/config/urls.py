2b3d7ad390bbee711de773d93f9800f5dfd369de
Travis Swicegood <development@domain51.com>
2011-05-21 22:56:04 -0500

    add in a urls.py

---
+++ armstrong/cli/templates/standard/config/urls.py
@@ -0,0 +1,25 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# ADMIN_BASE is the base URL for your Armstrong admin.  It is highly
+# recommended that you change this to a different URL unless you enforce a
+# strict password-strength policy for your users.
+ADMIN_BASE = "admin"
+
+
+# Comment the next two lines out to disnable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', '{{ project_name }}.views.home', name='home'),
+    # url(r'^{{ project_name }}/', include('{{ project_name }}.foo.urls')),
+
+    # Comment the admin/doc line below to disable admin documentation:
+    url(r'^%s/doc/' % ADMIN_BASE, include('django.contrib.admindocs.urls')),
+
+    # Comment the next line to disable the admin:
+    url(r'^%s/' % ADMIN_BASE, include(admin.site.urls)),
+)
+
