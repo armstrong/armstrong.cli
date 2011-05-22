d70ffd70555cf26228bbc638ca4c0e096057f296
Travis Swicegood <development@domain51.com>
2011-05-22 13:47:44 -0500

    make sure that the proper django-mptt gets installed

--- armstrong/cli/templates/standard/requirements/project.txt.py
+++ armstrong/cli/templates/standard/requirements/project.txt.py
@@ -1,1 +1,5 @@
 -e git://github.com/armstrongcms/armstrong.git#egg=armstrong
+
+# This is included here because the latest release of django-mptt does not have
+# the required code.
+-e git://github.com/django-mptt/django-mptt.git@0af02a95877041b2fd6d458bd95413dc1666c321#egg=django_mptt-dev
