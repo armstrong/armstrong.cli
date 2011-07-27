01dba0f7b734c8bb1d71f579daf19f95991ec6b0
Dave McLain <dmclain@texastribune.org>
2011-07-27 12:50:33 -0500

    Make init work when not using --demo

--- armstrong/cli/templates/standard/config/urls.py
+++ armstrong/cli/templates/standard/config/urls.py
@@ -26,10 +26,22 @@ urlpatterns = patterns('',
     url(r'^%s/' % ADMIN_BASE, include(admin.site.urls)),
 
     # Load the Armstrong "success" page by default
+    # {% if demo %}
     url(r'^$', QuerySetBackedWellView.as_view(well_title='front_page',
-                                              template_name="index.html",
+                                              template_name="front_page.html",
                                               queryset=Article.published.all()),
             name='front_page'),
+    # {% else %}
+    # Below is an example well view that might be used to display a well named
+    # 'front_page' allowing for placement of content on the home page. The view
+    # will work after # running `armstrong loaddata fixtures/initial_data.json`
+    #
+    #url(r'^$', QuerySetBackedWellView.as_view(well_title='front_page',
+    #                                          template_name="front_page.html",
+    #                                          queryset=Article.published.all()),
+    #        name='front_page'),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
+    # {% endif %}
 
     url(r'^section/(?P<full_slug>[-\w/]+)', 
             SimpleSectionView.as_view(template_name='section.html'),
