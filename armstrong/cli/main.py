10f554852e129e04eca52e9f64b70eeefd61ad28
Dave McLain <dmclain@texastribune.org>
2011-07-28 15:06:26 -0500

    making sure CLI checks installed apps

--- armstrong/cli/main.py
+++ armstrong/cli/main.py
@@ -4,8 +4,7 @@ import sys
 import argparse
 
 from .commands.init import init
-from .commands.import_wordpress import import_wordpress
-from django.core.management import execute_manager, get_commands
+from django.core.management import execute_manager, get_commands, setup_environ
 
 # TODO: use logging throughout for output
 CWD = os.getcwd()
@@ -16,6 +15,8 @@ def in_armstrong_project():
 
 def main():
     parser = argparse.ArgumentParser(description='Choose subcommand to run.')
+    parser.add_argument("--production", action='store_true',
+                        help='use config.production setting')
     subparsers = parser.add_subparsers(title='subcommands')
 
     from pkg_resources import iter_entry_points
@@ -33,12 +34,25 @@ def main():
             command.build_parser(armstrong_parser)
         armstrong_parser.set_defaults(func=command)
 
-    django_commands = get_commands().keys()
-    django_commands.sort()
-    for command in django_commands:
-        dj_parser = subparsers.add_parser(command, help='')
-        dj_parser.add_argument("--production", action='store_true')
-        dj_parser.set_defaults(func=call_django)
+    if in_armstrong_project():
+        # Make sure the current working dir is always in the path as the first
+        # element.  Initial tests on a Homebrew Python installation result in
+        # this not being the case.
+        if CWD not in sys.path:
+            sys.path.insert(0, CWD)
+
+        try:
+            settings_module = 'config.development'
+            __import__('config.development', globals(), locals())
+            setup_environ(sys.modules[settings_module])
+        except ImportError, e:
+            sys.stderr.write("Unable to import %s: %s\n" % (settings_module, e))
+            sys.exit(1)
+        django_commands = get_commands().keys()
+        django_commands.sort()
+        for command in django_commands:
+            dj_parser = subparsers.add_parser(command, help='')
+            dj_parser.set_defaults(func=call_django)
 
     args, argv = parser.parse_known_args()
     kwargs = vars(args)
@@ -49,16 +63,12 @@ def main():
         func(**kwargs)
 
 def call_django(argv=[], production=False):
-    # Make sure the current working dir is always in the path as the first
-    # element.  Initial tests on a Homebrew Python installation result in
-    # this not being the case.
     if CWD not in sys.path:
         sys.path.insert(0, CWD)
-
     settings_module = "config.development"
     if production:
         settings_module = "config.production"
-
+    settings = None
     try:
         __import__(settings_module, globals(), locals())
         settings = sys.modules[settings_module]
