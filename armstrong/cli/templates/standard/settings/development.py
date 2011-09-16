56fb3296419e1a1e86530f9691d64ee58e04ba6a
crccheck <c@crccheck.com>
2011-09-15 20:25:43 -0500

    fix demo app so sqlite database is relative to project_dir

--- armstrong/cli/templates/standard/settings/development.py
+++ armstrong/cli/templates/standard/settings/development.py
@@ -9,7 +9,7 @@ DATABASES = {
     'default': {
         #{% if demo %}
         'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': './demo/demo_database.sqlite3',                       # Or path to database file if using sqlite3.
+        'NAME': project_dir('./demo/demo_database.sqlite3'),                       # Or path to database file if using sqlite3.
         #{% else %}
         'ENGINE': 'django.db.backends.',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '',                       # Or path to database file if using sqlite3.
