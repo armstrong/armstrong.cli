5498d35aabfc8d17e49ab62f1dd4126777a842be
Travis Swicegood <development@domain51.com>
2011-09-28 11:44:57 -0500

    Extract method
    Been meaning to make this change for awhile.

--- armstrong/cli/templates/standard/settings/defaults.py
+++ armstrong/cli/templates/standard/settings/defaults.py
@@ -1,10 +1,7 @@
 # Django settings for {{ project_name }} project.
 import os
 
-
-def project_dir(*paths):
-    base = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
-    return os.path.join(base, *paths)
+from .helpers import project_dir
 
 
 DEBUG = False
